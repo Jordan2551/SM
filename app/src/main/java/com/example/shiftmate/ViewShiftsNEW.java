fbaeaf0fccdd8d2b11858e2927b7e1d9af0d4841
Jordan Cohen <jordan1158@gmail.com>
2016-06-07 21:46:07 +0300

    Added NewShiftActivityNEW

---
+++ app/src/main/java/com/example/shiftmate/ViewShiftsNEW.java
@@ -0,0 +1,13 @@
+package com.example.shiftmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewShiftsNEW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_shifts_new);
+    }
+}
